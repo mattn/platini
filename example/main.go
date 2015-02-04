7d912e1a13527c9039d25d6563e55f20da916366
mattn <mattn.jp@gmail.com>
2015-02-04 19:46:45 +0900

    Update exmample

--- example/main.go
+++ example/main.go
@@ -17,19 +17,19 @@ type PetReq struct {
 	Id int `json:"id"`
 }
 
+var pets = []Pet{{1, "Linda_pp", "犬"}, {2, "supermomonga", "ももんが"}}
+
 func list() ([]Pet, error) {
-	return []Pet{{1, "Linda_pp", "犬"}, {2, "supermomonga", "ももんが"}}, nil
+	return pets, nil
 }
 
 func get(req *PetReq) (*Pet, error) {
-	switch req.Id {
-	case 1:
-		return &Pet{1, "Linda_pp", "犬"}, nil
-	case 2:
-		return &Pet{2, "supermomonga", "モモンガ"}, nil
-	default:
-		return nil, fmt.Errorf("user not found: id=%d", req.Id)
+	for _, pet := range pets {
+		if req.Id == pet.Id {
+			return &pet, nil
+		}
 	}
+	return nil, fmt.Errorf("user not found: id=%d", req.Id)
 }
 
 func add(w http.ResponseWriter, r *http.Request, req *Pet) (*Pet, error) {
@@ -39,7 +39,9 @@ func add(w http.ResponseWriter, r *http.Request, req *Pet) (*Pet, error) {
 	if strings.TrimSpace(req.Name) == "" {
 		return nil, fmt.Errorf("invalid request: name is require")
 	}
-	return &Pet{1, "Linda_pp", "犬"}, nil
+	req.Id = len(pets) + 1
+	pets = append(pets, *req)
+	return req, nil
 }
 
 func main() {
